76e753df2998592b6a368be23359437f2470907c
svl <svl@foreach.be>
2016-11-23 19:34:21 +0100

    better UT

---
+++ neighbournet-api/src/test/java/be/sandervl/neighbournet/service/crawler/SiteCrawlerTest.java
@@ -0,0 +1,21 @@
+package be.sandervl.neighbournet.service.crawler;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author: sander
+ * @date: 23/11/2016
+ */
+public class SiteCrawlerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void visit() throws Exception {
+
+    }
+
+}
